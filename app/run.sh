7f8b7cd3027c30eab73f1d38771c2e39c3b2d368
Sharad Pratap <sh4r4d@gmail.com>
2017-06-13 03:03:50 +0530

    Improvement to take source configuration directly from source container's
    DB_ environment variables, which will be easier to maintain with container
    itself.
    
    added
    
    - DB_DESTINATION for destination type
    - DB_DESTPATH    for destination_directory_name
    - DB_VOLUMES     for volume dirs to take backup
    - DB_BACKUP_PRECMD="mysqldump all > /data/dump.sql"
      for cmd to run using exec before taking backup
    - DB_BACKUP_POSTCMD="echo done"
      for cmd to run using exec after taking backup
    - DB_RESTORE_PRECMD="echo run restore"
      for cmd to run using exec before restoring
    - DB_RESTORE_POSTCMD="echo done"
      for cmd to run using exec after restoring

--- app/run.sh
+++ app/run.sh
@@ -4,21 +4,112 @@ set -eux
 
 source $(dirname $0)/common.sh
 
+echo _NOTE: common
+
 check_vitals
 
+echo _NOTE: check_vitals
+
 install_docker
+
+echo _NOTE: install_docker
 
 THIS_CONTAINER_ID="$(cat /proc/self/cgroup | grep 'docker/' | sed 's/^.*\///' | tail -n1)"
 THIS_DOCKER_IMAGE="$(docker inspect ${THIS_CONTAINER_ID} | underscore extract 0.Config.Image --outfmt text)"
 # TODO find the image tag
 
-destinations=$(cat ${CONFIG_FILE} | $BIN_SHYAML keys destinations)
-sources=$(cat ${CONFIG_FILE} | $BIN_SHYAML keys sources)
-for source in $sources
+
+
+if cat ${CONFIG_FILE} | $BIN_SHYAML keys sources
+then
+    destinations=$(cat ${CONFIG_FILE} | $BIN_SHYAML keys destinations)
+    sources=$(cat ${CONFIG_FILE} | $BIN_SHYAML keys sources)
+
+    for source in $sources
+    do
+        type=$(get_source_parameter $source type)
+        # container, compose or docker-cloud ?
+        source_container_id=$(get_container_id_from_config source $source)
+
+        if [ -z "${source_container_id}" ]; then
+            echo "!! No container found with this name! Aborting."
+            exit 1
+        fi
+        if [ $(echo ${source_container_id} | wc -l) -gt 1 ]; then
+            echo "!! More than 1 container with this name! Aborting."
+            exit 2
+        fi
+        source_container_name=$(docker_get_container_name_from_id ${source_container_id})
+
+        destination=$(get_source_parameter $source destination)
+        config_destination_server=$(get_destination_parameter $destination server "")
+
+        # Networking
+        network_opts=""
+        if [ -z "$config_destination_server" ]; then
+            destination_container_id=$(get_container_id_from_config destination $destination)
+            if [ -n "${destination_container_id}" -a "$(echo ${destination_container_id} | wc -l)" -eq 1 ]; then
+                network_opts="--network=container:${destination_container_id}"
+            fi
+        fi
+
+        # If the config file is mounted from host or other container, find the way to get it to workers containers
+        config_file_volume_opts=""
+
+        config_file_hostpath=$(docker inspect ${THIS_CONTAINER_ID} | underscore extract 0.HostConfig.Binds --outfmt text | grep -E ":${CONFIG_FILE}:" | cut -d":" -f 1)
+        if [ -n "${config_file_hostpath}" ]; then
+            config_file_volume_opts="-v ${config_file_hostpath}:${CONFIG_FILE}:ro"
+        fi
+        if [ -z "${config_file_hostpath}" ]; then
+            # find if mounted from other container
+            mount_points="$(docker inspect ${THIS_CONTAINER_ID} | underscore extract 0.Mounts)"
+            mount_points_count=$(echo "${mount_points}" | underscore process 'data.length')
+            i="0"
+            while [ $i -lt $mount_points_count ];
+            do
+                tmp_destination=$(echo "${mount_points}" | underscore extract "$i.Destination" --outfmt text)
+                if [ "$tmp_destination" == "$(dirname ${CONFIG_FILE})" ]; then
+                    config_file_hostpath=$(echo "${mount_points}" | underscore extract "$i.Source" --outfmt text);
+                    break
+                fi
+                unset tmp_destination
+                i=$[$i+1]
+            done
+            unset i mount_points mount_points_count tmp_destination
+
+            if [ -n "$config_file_hostpath" ]; then
+                config_file_volume_opts="-v ${config_file_hostpath}:$(dirname ${CONFIG_FILE}):ro"
+            fi
+        fi
+        unset config_file_hostpath
+
+        # Give the worker container the variables that override config file
+        environment_opts=""
+        ENV_VARIABLES=$(env | grep "^CONFIG_")
+        for config_var in ${ENV_VARIABLES}
+        do
+            environment_opts="${environment_opts} -e ${config_var}"
+        done
+
+        echo "Launching Worker for source $source"
+        eval docker run --rm --volumes-from ${source_container_id} \
+             --name docker-backup-worker-${source_container_name} \
+             ${environment_opts} \
+             ${network_opts} \
+             ${config_file_volume_opts} \
+             -v ${DOCKER_SOCKET}:${DOCKER_SOCKET}:ro \
+             $THIS_DOCKER_IMAGE worker-config $source
+    done
+
+fi # if cat ${CONFIG_FILE} | $BIN_SHYAML keys sources
+
+echo _NOTE: INSTNACE
+docker ps -q
+
+
+for cid in $(docker ps -q)
 do
-    type=$(get_source_parameter $source type)
-    # container, compose or docker-cloud ?
-    source_container_id=$(get_container_id_from_config source $source)
+    source_container_id=$cid    # hex
 
     if [ -z "${source_container_id}" ]; then
         echo "!! No container found with this name! Aborting."
@@ -30,64 +121,77 @@ do
     fi
     source_container_name=$(docker_get_container_name_from_id ${source_container_id})
 
-    destination=$(get_source_parameter $source destination)
-    config_destination_server=$(get_destination_parameter $destination server "")
+    echo _NOTE: source_container_name=$source_container_name
 
-    # Networking
-    network_opts=""
-    if [ -z "$config_destination_server" ]; then
-        destination_container_id=$(get_container_id_from_config destination $destination)
-        if [ -n "${destination_container_id}" -a "$(echo ${destination_container_id} | wc -l)" -eq 1 ]; then
-            network_opts="--network=container:${destination_container_id}"
-        fi
-    fi
+    destination=$(docker_get_db_destination_from_id $source_container_name)
 
-    # If the config file is mounted from host or other container, find the way to get it to workers containers
-    config_file_volume_opts=""
+    echo _NOTE: destination=$destination
 
-    config_file_hostpath=$(docker inspect ${THIS_CONTAINER_ID} | underscore extract 0.HostConfig.Binds --outfmt text | grep -E ":${CONFIG_FILE}:" | cut -d":" -f 1)
-    if [ -n "${config_file_hostpath}" ]; then
-        config_file_volume_opts="-v ${config_file_hostpath}:${CONFIG_FILE}:ro"
-    fi
-    if [ -z "${config_file_hostpath}" ]; then
-        # find if mounted from other container
-        mount_points="$(docker inspect ${THIS_CONTAINER_ID} | underscore extract 0.Mounts)"
-        mount_points_count=$(echo "${mount_points}" | underscore process 'data.length')
-        i="0"
-        while [ $i -lt $mount_points_count ];
-        do
-            tmp_destination=$(echo "${mount_points}" | underscore extract "$i.Destination" --outfmt text)
-            if [ "$tmp_destination" == "$(dirname ${CONFIG_FILE})" ]; then
-                config_file_hostpath=$(echo "${mount_points}" | underscore extract "$i.Source" --outfmt text);
-                break
+    if [ ! -z "$destination" ]
+    then
+        echo _NOTE: passed
+
+        config_destination_server=$(get_destination_parameter $destination server "")
+
+        # Networking
+        network_opts=""
+        if [ -z "$config_destination_server" ]; then
+            destination_container_id=$(get_container_id_from_config destination $destination)
+            if [ -n "${destination_container_id}" -a "$(echo ${destination_container_id} | wc -l)" -eq 1 ]; then
+                network_opts="--network=container:${destination_container_id}"
             fi
-            unset tmp_destination
-            i=$[$i+1]
-        done
-        unset i mount_points mount_points_count tmp_destination
-
-        if [ -n "$config_file_hostpath" ]; then
-            config_file_volume_opts="-v ${config_file_hostpath}:$(dirname ${CONFIG_FILE}):ro"
         fi
+
+        # If the config file is mounted from host or other container, find the way to get it to workers containers
+        config_file_volume_opts=""
+
+        config_file_hostpath=$(docker inspect ${THIS_CONTAINER_ID} | underscore extract 0.HostConfig.Binds --outfmt text | grep -E ":${CONFIG_FILE}:" | cut -d":" -f 1)
+        if [ -n "${config_file_hostpath}" ]; then
+            config_file_volume_opts="-v ${config_file_hostpath}:${CONFIG_FILE}:ro"
+        fi
+        if [ -z "${config_file_hostpath}" ]; then
+            # find if mounted from other container
+            mount_points="$(docker inspect ${THIS_CONTAINER_ID} | underscore extract 0.Mounts)"
+            mount_points_count=$(echo "${mount_points}" | underscore process 'data.length')
+            i="0"
+            while [ $i -lt $mount_points_count ];
+            do
+                tmp_destination=$(echo "${mount_points}" | underscore extract "$i.Destination" --outfmt text)
+                if [ "$tmp_destination" == "$(dirname ${CONFIG_FILE})" ]; then
+                    config_file_hostpath=$(echo "${mount_points}" | underscore extract "$i.Source" --outfmt text);
+                    break
+                fi
+                unset tmp_destination
+                i=$[$i+1]
+            done
+            unset i mount_points mount_points_count tmp_destination
+
+            if [ -n "$config_file_hostpath" ]; then
+                config_file_volume_opts="-v ${config_file_hostpath}:$(dirname ${CONFIG_FILE}):ro"
+            fi
+        fi
+        unset config_file_hostpath
+
+        # Give the worker container the variables that override config file
+        environment_opts=""
+        ENV_VARIABLES=$(env | grep "^CONFIG_")
+        for config_var in ${ENV_VARIABLES}
+        do
+            environment_opts="${environment_opts} -e ${config_var}"
+        done
+
+        echo "Launching Worker for source $source_container_name"
+        eval docker run --rm --volumes-from ${source_container_id} \
+             --name docker-backup-worker-${source_container_name} \
+             ${environment_opts} \
+             ${network_opts} \
+             ${config_file_volume_opts} \
+             -v ${DOCKER_SOCKET}:${DOCKER_SOCKET}:ro \
+             $THIS_DOCKER_IMAGE worker-env $source_container_name
+    else
+        echo _NOTE: failed
+        echo No destination were present for $source_container_name
     fi
-    unset config_file_hostpath
-
-    # Give the worker container the variables that override config file
-    environment_opts=""
-    ENV_VARIABLES=$(env | grep "^CONFIG_")
-    for config_var in ${ENV_VARIABLES}
-    do
-        environment_opts="${environment_opts} -e ${config_var}"
-    done
-
-    echo "Launching Worker for source $source"
-    eval docker run --rm --volumes-from ${source_container_id} \
-        --name docker-backup-worker-${source_container_name} \
-        ${environment_opts} \
-        ${network_opts} \
-        ${config_file_volume_opts} \
-        -v ${DOCKER_SOCKET}:${DOCKER_SOCKET}:ro \
-        $THIS_DOCKER_IMAGE worker $source
 done
 
 exit 0
---
+++ app/worker-env.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+
+set -eux
+
+source $(dirname $0)/common.sh
+
+install_docker
+
+# source=$1
+source_container_id=$1
+restore=${2:-0}
+
+if [ "$restore" == "restore" -a "$(dir_is_mounted_from_host /docker-restore)" != "0" ]
+then
+    echo "!!! In restore mode but the /docker-restore dir is not mounted from host" >&2
+    exit 1
+fi
+
+# source_container_id=$(get_container_id_from_config source $source)
+source_container_name=$(docker_get_container_name_from_id $source_container_id)
+destination_directory_name=$(docker_get_db_destpath_from_id $source_container_id "${source_container_name}")
+
+declare -a volumes_to_backup
+
+
+
+volumes_path=$(docker_get_db_volumes_from_id $source_container_id)
+echo _NOTE: volumes_path=$volumes_path
+# IFS=':' volumes=($volumes_path)
+volumes=( $(echo $volumes_path | sed 's/:/ /g' ) )
+echo _NOTE: volumes=$volumes
+
+i=1
+for volume in "${volumes[@]}"
+do
+    volumes_to_backup[$i]="$volume"
+    i=$(($i+1))
+done
+
+
+destination=$(docker_get_db_destination_from_id $source_container_name)
+
+if [ ! -z "$destination" ]
+then
+
+    echo _NOTE: destination=$destination
+
+
+    BACKUP_METHOD=$(get_destination_parameter $destination type)
+    BACKUP_METHOD_PARAMS=""
+    BACKUP_KEEP_N_FULL=$(get_parameter backup_keep_n_full)
+
+    par2_prefix=""
+    par2_redundancy_opt=""
+    PAR2_ENABLED=$(get_parameter par2.enabled false)
+    if [ "${PAR2_ENABLED}" == "True" ]
+    then
+        par2_prefix="par2+"
+        par2_redundancy_opt="--par2-redundancy $(get_parameter par2.redundancy 10)"
+    fi
+
+    BACKUP_METHOD_PARAMS="${BACKUP_METHOD_PARAMS} ${par2_redundancy_opt}"
+
+    if [ "${BACKUP_METHOD}" == "ftp" ]
+    then
+        server=$(get_destination_parameter $destination server "")
+        if [ -z "${server}" ]; then
+            server_id=$(get_container_id_from_config destination $destination)
+            server=$(docker_get_container_name_from_id $server_id)
+        fi
+        port=$(get_destination_parameter $destination port 21)
+        username=$(get_destination_parameter $destination username)
+        path=$(get_destination_parameter $destination path /)
+        BACKUP_URL="${par2_prefix}ftp://${username}@${server}:${port}/${path}/${destination_directory_name}"
+        export FTP_PASSWORD=$(get_destination_parameter $destination password "")
+    fi
+
+    if [ "${BACKUP_METHOD}" == "s3" ]
+    then
+        export AWS_ACCESS_KEY_ID=$(get_destination_parameter $destination access_key_id)
+        export AWS_SECRET_ACCESS_KEY=$(get_destination_parameter $destination secret_access_key)
+        AWS_REGION=$(get_destination_parameter $destination region)
+        AWS_BUCKET_NAME=$(get_destination_parameter $destination bucket_name)
+        BACKUP_URL="${par2_prefix}s3://s3.${AWS_REGION}.amazonaws.com/${AWS_BUCKET_NAME}/${destination_directory_name}"
+
+        BACKUP_METHOD_PARAMS="${BACKUP_METHOD_PARAMS} --s3-european-buckets --s3-use-new-style"
+        if [ "$(get_destination_parameter $destination use_ia False)" == "True" ];
+        then
+            BACKUP_METHOD_PARAMS="${BACKUP_METHOD_PARAMS} --s3-use-ia"
+        fi
+    fi
+
+    if [ "${BACKUP_METHOD}" == "sftp" ]
+    then
+        server=$(get_destination_parameter $destination server "")
+        if [ -z "${server}" ]; then
+            server_id=$(get_container_id_from_config destination $destination)
+            server=$(docker_get_container_name_from_id $server_id)
+        fi
+        port=$(get_destination_parameter $destination port 21)
+        username=$(get_destination_parameter $destination username)
+        password=$(get_destination_parameter $destination password)
+        path=$(get_destination_parameter $destination path /)
+        BACKUP_URL="${par2_prefix}sftp://${username}@${server}:${port}/${path}/${destination_directory_name}"
+        export FTP_PASSWORD=$(get_destination_parameter $destination password "")
+        mkdir -p ~/.ssh
+        ssh-keyscan -p ${port} ${server} >> ~/.ssh/known_hosts
+    fi
+
+    for volume_to_backup in "${volumes_to_backup[@]}"
+    do
+        if [ -d "${volume_to_backup}" ]
+        then
+            if [ "$restore" == "restore" ];
+            then
+                docker_run_cmd_in_container $source_container_id $(docker_get_db_restore_precmd_from_id $source_container_id)
+
+                NOW=$(date +"%Y-%m-%d-%H%M%S")
+                restore_destination="/docker-restore/restore-${NOW}"
+                duplicity --no-encryption "${BACKUP_URL}" "${restore_destination}/${destination_directory_name}"
+
+                docker_run_cmd_in_container $source_container_id $(docker_get_db_restore_postcmd_from_id $source_container_id)
+
+                exit 0
+            else
+                docker_run_cmd_in_container $source_container_id $(docker_get_db_backup_precmd_from_id $source_container_id)
+
+                duplicity --full-if-older-than "$(get_parameter backup_full_if_older_than)" \
+                          --no-encryption --allow-source-mismatch \
+                          ${BACKUP_METHOD_PARAMS} \
+                          "${volume_to_backup}" "${BACKUP_URL}"
+
+                docker_run_cmd_in_container $source_container_id $(docker_get_db_backup_postcmd_from_id $source_container_id)
+            fi
+        else
+            echo No "${volume_to_backup}" such directory exist for ${restore:-backup}.
+        fi
+    done
+
+    duplicity remove-all-but-n-full --force --no-encryption "${BACKUP_KEEP_N_FULL}" "${BACKUP_URL}"
+
+else
+    echo No destination were present for $source_container_name
+fi # if [ ! -z "$destination" ]
+
+exit 0
